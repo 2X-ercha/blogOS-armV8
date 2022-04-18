c3dc7183ee660987b13646c61652b3d89eb0395b
noionion <noionion@outlook.com>
2022-04-18 09:32:08 +0800

    lab1

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+// 不使用标准库
+#![no_std]
+// 不使用预定义入口点
+#![no_main]
+#![feature(global_asm)]
+
+use core::ptr;
+
+mod panic;
+
+global_asm!(include_str!("start.s"));
+
+#[no_mangle] // 不修改函数名
+pub extern "C" fn not_main() {
+const UART0: *mut u8 = 0x0900_0000 as *mut u8;
+    let out_str = b"AArch64 Bare Metal";
+    for byte in out_str {
+        unsafe {
+            ptr::write_volatile(UART0, *byte);
+        }
+    }
+}
+
